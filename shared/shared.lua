7ae43f7f1afefa0fa64c95a24918577fd059aa53
Pyth3rEx <pyth3rex@outlook.com>
2024-03-02 00:02:04 +0100

    Added exports

---
+++ shared/shared.lua
@@ -0,0 +1,19 @@
+exports('onFireStart', function ()
+    
+end)
+
+exports('onFireStop', function ()
+    
+end)
+
+exports('onFireSpread', function ()
+    
+end)
+
+exports('onPlayerOnDuty', function ()
+    
+end)
+
+exports('onPlayerOffDuty', function ()
+    
+end)
