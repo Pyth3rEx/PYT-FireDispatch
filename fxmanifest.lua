bf4e246a2ecb534c31dd25e3452366433c689166
Pyth3rEx <pyth3rex@outlook.com>
2024-03-01 23:26:54 +0100

    added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Pyth3rEx'
+description 'PYT-FireDispatch resource'
+version '0.0.0'
+
+client_scripts {
+    'client/mainClient.lua'
+}
+
+server_scripts {
+    'server/mainServer.lua'
+}
+
+shared_scripts {
+    'shared/shared.lua'
+}
+
+files {
+    'config.lua'
+}
