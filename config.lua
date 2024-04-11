2dbe20c34a40bcf796a9d069666bcbfff25bf925
Pyth3rEx <pyth3rex@outlook.com>
2024-04-11 16:39:35 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Core{
-    Frameworks = {'qb-core, esx, standalone'},
+    Frameworks = {'qb-core', 'esx', 'standalone'},
     UseInventory = {'qb-inventory', 'qs-inventory' , 'ox_inventory'},
     UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add setr UseTarget true)
 }
